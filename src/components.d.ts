8dfd75aa5b363d4599b635284f0426a28a29d799
lishuo <2449906403@qq.com>
2023-10-28 09:40:22 +0800

    feature: 增加ai对话页面

--- src/components.d.ts
+++ src/components.d.ts
@@ -21,7 +21,6 @@ declare module 'vue' {
     AInputSearch: typeof import('ant-design-vue/es')['InputSearch']
     AMenu: typeof import('ant-design-vue/es')['Menu']
     AMenuItem: typeof import('ant-design-vue/es')['MenuItem']
-    AModal: typeof import('ant-design-vue/es')['Modal']
     ARadio: typeof import('ant-design-vue/es')['Radio']
     ARadioGroup: typeof import('ant-design-vue/es')['RadioGroup']
     ARow: typeof import('ant-design-vue/es')['Row']
